e1a81638429d8f1094f0a7c345d2e3967aafc373
Muhammad Arshad <muhd.arshadra@gmail.com>
2024-10-24 21:04:44 +0530

    Update dom.py

--- dom.py
+++ dom.py
@@ -1,10 +1,27 @@
 # dom.py (hypothetical)
 
 class Dom:
-    def get(self, element_id):
-        # In a browser environment, you'd interact with JavaScript here
+    def get_element(self, element_id):
+        """
+        Simulate fetching a DOM element by its ID.
+        
+        Args:
+            element_id (str): The ID of the DOM element to fetch.
+        
+        Returns:
+            str: A message indicating the element being fetched.
+        """
         return f"Fetching DOM element with ID: {element_id}"
     
-    def onClick(self, element_id, callback):
-        # Set up a hypothetical event listener
+    def set_on_click(self, element_id, callback):
+        """
+        Simulate setting up a click event listener on a DOM element.
+        
+        Args:
+            element_id (str): The ID of the DOM element to attach the event to.
+            callback (function): The callback function to be called on click.
+        
+        Returns:
+            str: A message indicating the click event setup.
+        """
         return f"Setting click event for element {element_id}"
